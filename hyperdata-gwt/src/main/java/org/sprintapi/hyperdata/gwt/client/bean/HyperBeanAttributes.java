e8c5f4186da901045ef9e8d90c820ef45ffbf498
Filip <filip26@gmail.com>
2013-12-20 00:17:29 +0100

    update HyperBeanAttributes

--- hyperdata-gwt/src/main/java/org/sprintapi/hyperdata/gwt/client/bean/HyperBeanAttributes.java
+++ hyperdata-gwt/src/main/java/org/sprintapi/hyperdata/gwt/client/bean/HyperBeanAttributes.java
@@ -2,4 +2,22 @@ package org.sprintapi.hyperdata.gwt.client.bean;
 
 public class HyperBeanAttributes {
 
+	private String[] profile;
+
+	public HyperBeanAttributes() {
+		this(null);
+	}
+
+	public HyperBeanAttributes(String[] profile) {
+		super();
+		this.profile = profile;
+	}
+
+	public String[] getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String[] profile) {
+		this.profile = profile;
+	}
 }
